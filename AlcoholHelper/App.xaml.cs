a0ab813add67262dde2e6bdf95ca3da68d25c084
manderson116 <manderson116@my.wctc.edu>
2024-11-04 14:39:15 -0600

    Add project files.

---
+++ AlcoholHelper/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AlcoholHelper
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AlcoholHelper/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AlcoholHelper
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
